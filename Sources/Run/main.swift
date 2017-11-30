5f8c858adf7ffa524dfa865d1ef07d69601e63c3
Łukasz Przytuła <lprzytula@gmail.com>
2017-11-30 09:58:20 +0100

    Disabled logging to file

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -19,7 +19,7 @@ import App
 let config = try Config()
 try config.setup()
 
-let drop = try Droplet(config: config, log: FileLogger())
+let drop = try Droplet(config)
 try drop.setup()
 
 try drop.run()
